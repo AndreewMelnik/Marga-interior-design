a91f8b0e9745072c91b79fb5a0b8a90a83972082
AndreewMelnik <andreew.mel@gmail.com>
2022-09-19 22:25:36 +0300

    122

---
+++ src/components/PreviewSlider.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-carousel
+      cycle
+      height="800"
+      hide-delimiter-background
+      show-arrows-on-hover
+  >
+    <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+        :src="slide.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+    >
+        <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+        >
+        </v-row>
+    </v-carousel-item>
+    <div class="slider__box">
+      <h1>Design That Makes You Feel At Home</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti similique, nesciunt.</p>
+      <p><a href="#" class="btn">Contact Us</a></p>
+    </div>
+  </v-carousel>
+</template>
+btn-primary
+
+<script>
+export default {
+  name: "PreviewSlider",
+  inject: ['isScrollable', 'isMobile'],
+  data () {
+    return {
+      slides: [
+        {
+          src: require("../assets/images/hero_1.jpg"),
+        },
+        {
+          src: require("../assets/images/hero_2.jpg"),
+        },
+        {
+          src: require("../assets/images/hero_3.jpg"),
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+.slider__box{
+  z-index: 2;
+  background: #fff;
+  padding: 4rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  max-width: 500px;
+  height:455px
+}
+.slider__box h1{
+  font-family: "Allrounder Grotesk Regular";
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  font-size: 2.5rem;
+  color: #364d59;
+}
+.slider__box p{
+  line-height: 1.7;
+  color: #364d59 ;
+  margin-bottom: 3rem;
+}
+.btn {
+  color: #fff;
+  background-color: #43ab92;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  user-select: none;
+  border: 1px solid #43ab92;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  text-decoration: none;
+}
+
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,57 @@
+27 lines (24 sloc) 646 Bytes
+<template>
+  <v-app-bar  class="navbar" color="white">
+<!--    <v-btn icon>-->
+<!--      <v-icon >fas fa-dragon</v-icon>-->
+<!--    </v-btn>-->
+
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')">Home</v-btn>
+    <v-btn text @click="scroll('project')">Projects</v-btn>
+    <v-btn text @click="scroll('services')">Services</v-btn>
+    <div class="logo">
+      <span>marga</span>
+    </div>
+    <v-btn text @click="scroll('about')">About</v-btn>
+    <v-btn text @click="scroll('blog')"> Blog</v-btn>
+    <v-btn text @click="scroll('contact')"> Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
+
+<style>
+.navbar{
+display: flex;
+  justify-content: center;
+  padding: 20px;
+  max-height: 100px;
+}
+.v-btn{
+  text-transform: none;
+  font-family: "AcuminPro-Medium";
+}
+.v-btn.v-size--default {
+  font-size: 16px;
+}
+.logo{
+  font-family: "Acumin Pro Regular";
+  color:#43ab92;
+  font-size: 32px;
+  width:300px;
+  text-align: center;
+}
+.logo span{
+  vertical-align: super;
+}
+
+</style>
