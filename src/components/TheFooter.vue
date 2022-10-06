48f613d1cd6aba1a0964b2a926796bb09db0b438
AndreewMelnik <andrew.mel@mail.ru>
2022-10-06 22:19:35 +0300

    footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,59 +1,67 @@
 <template>
   <v-footer
-      color="primary lighten-1"
-      padless
+      color="#c9ccd4"
+      class="pt-5 pb-5"
   >
+    <v-container
+        class="pa-10">
         <v-row
         justify="center"
-        no-gutters
     >
-
+          <v-col class="about">
+            <h3>About Us </h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.<p/>
+          </v-col>
       <v-col
           class="py-4 text-center"
-
           cols="12"
-          sm="4"
+          sm="1"
       >
-
+        <h3 class="features">
+          Features
+        </h3>
           <v-btn
               v-for="link in links"
               :key="link"
-
               text
               rounded
-              class="my-2"
+              class="ml-16"
+              color="rgba(0, 0, 0, 0.5)"
+
           >
+
             {{ link }}
           </v-btn>
 
 
 
       </v-col>
-          <v-col>
-            About Us
-            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
-          </v-col>
-          <v-col>
-            <form>
-              <v-text-field
-                  v-model="email"
-                  :error-messages="emailErrors"
-                  label="E-mail"
-                  required
-                  @input="$v.email.$touch()"
-                  @blur="$v.email.$touch()"
-              ></v-text-field>
 
-              <v-btn
-                  class="mr-4"
-                  @click="submit"
-              >
-                submit
-              </v-btn>
-            </form>
+          <v-col>
+
           </v-col>
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          <form>
+            <h3 class="footer-heading mb-4">Subscribe to Newsletter</h3>
+            <v-text-field
+                background-color="white"
+                height="55px"
+                v-model="email"
+                :error-messages="emailErrors"
+                label="Enter E-mail"
+                required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-btn
+                class="mr-4"
+                @click="submit"
+            >
+              submit
+            </v-btn>
+          </form>
     </v-row>
+    </v-container>
   </v-footer>
 </template>
 
@@ -63,7 +71,6 @@ export default {
 
   data: () => ({
   links: [
-  'Home',
   'About Us',
   'Team',
   'Services',
@@ -75,8 +82,34 @@ export default {
 </script>
 
 <style scoped>
+.about{
+font-size: 16px;
+padding-bottom: 20px;
+  padding-top: 20px;
+}
+.about h3 {
+  margin-bottom: 20px;
+}
+.about p {
+  color: gray;
+  margin-bottom: 20px;
+}
+.features{
+  padding-left: 80px;
+  padding-bottom: 10px;
+}
+v-text-field{
+  color: white;
+}
 
+/*.v-input__slot{*/
+/*  position: relative;*/
+/*  flex: 1 1 auto;*/
+/*  width: 1%;*/
+/*  margin-bottom: 0;*/
+/*  height: 55px;*/
+/*  background-color: #fff ;*/
+/*}*/
 </style>
-
 
 
